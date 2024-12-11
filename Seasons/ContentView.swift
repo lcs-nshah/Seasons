3b3f31a9eb50a23c73334966b95f2029cd77177b
Neil Shah <nshah@lcs.on.ca>
2024-12-10 23:28:16 -0500

    Initial Commit

---
+++ Seasons/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Seasons
+//
+//  Created by Neil Shah on 2024-12-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Seasons/SeasonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeasonsApp.swift
+//  Seasons
+//
+//  Created by Neil Shah on 2024-12-10.
+//
+
+import SwiftUI
+
+@main
+struct SeasonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
